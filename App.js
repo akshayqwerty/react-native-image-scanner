57cb5099ce39abe9fef16845eb5169b68bc91dbf
AKSHAY KUMAR SINGH <akshay_kumar@qwertycode.in>
2022-03-04 19:05:56 +0530

    add image scanner

---
+++ App.js
@@ -0,0 +1,34 @@
+import {StyleSheet, Text, View, TouchableOpacity, Button} from 'react-native';
+import React from 'react';
+import {launchImageLibrary} from 'react-native-image-picker';
+import RNQRGenerator from 'rn-qr-generator';
+
+export default function App() {
+  const openGallery = () => {
+    launchImageLibrary({}, response => {
+      console.log('Response = ', response.assets[0].uri);
+      RNQRGenerator.detect({
+        uri: response.assets[0].uri,
+      })
+        .then(response => {
+          const {values} = response; // Array of detected QR code values. Empty if nothing found.
+          console.log(values);
+        })
+        .catch(error => console.log('Cannot detect QR code in image', error));
+    });
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Button title="Choose Image" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
